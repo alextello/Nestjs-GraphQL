c8cbd005c451e0b7544a28f3f1d779f75b6ad2ff
unknown <alextello1@hotmail.com>
2020-08-13 20:33:03 -0600

    Creando una leccion

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,15 +1,27 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
+import { Lesson } from './lesson.entity';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
   @Query(returns => LessonType)
-  lesson() {
+  lesson(): LessonType {
     return {
       id: 'sdf3fj329',
       name: 'matematicas',
       startDate: new Date().toISOString(),
       endDate: new Date().toISOString(),
     };
+  }
+
+  @Mutation(returns => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ): Promise<Lesson> {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
